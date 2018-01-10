8d90da843fdee0a3e42afadf4c15382cc688217a
saumya-singh <saumya.singh0993@gmail.com>
2018-01-10 17:13:10 +0530

    access the database to get all the tasks of a user

--- db_handler.py
+++ db_handler.py
@@ -1,8 +1,8 @@
-import pymongo
+import pymongo, json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson import Binary, Code
-from bson.json_util import dumps
+from bson.json_util import dumps, RELAXED_JSON_OPTIONS, DatetimeRepresentation
 
 def insert_to_db(task):
     client = MongoClient()
@@ -10,6 +10,30 @@ def insert_to_db(task):
     try:
         return_values = db.tasks.insert_one(task)
         id = return_values.inserted_id
-        return dumps(db.tasks.find_one({"_id" : ObjectId(id)}))
+        task_details = dumps(db.tasks.find_one({"_id" : ObjectId(id)}), \
+                        json_options = RELAXED_JSON_OPTIONS)
+        formatted_task_details = formatting_func(json.loads(task_details))
+        return [formatted_task_details, 1]
     except:
-        return "exception"
+        return ["Cannot add task in the ToDo list", 0]
+
+def list_tasks_from_db(userid):
+    client = MongoClient()
+    db = client.todo_database
+    task_list = []
+    count = db.tasks.find({"created_by" : userid}).count()
+    if count == 0:
+        return ["No task added from this userid", 1]
+    cursor = db.tasks.find({"created_by" : userid})
+    for each_task in cursor:
+        each_task_details = dumps(each_task, json_options = RELAXED_JSON_OPTIONS)
+        formatted_task_details = formatting_func(json.loads(each_task_details))
+        task_list.append(formatted_task_details)
+    print(task_list)
+    return [task_list, 1]
+
+def formatting_func(data):
+    data["_id"] = data["_id"]["$oid"]
+    data["created_time"] = data["created_time"]["$date"]
+    data["deadline"] = data["deadline"]["$date"]
+    return data
