b818dd5dc196257df5fd22b7a0942253343ede99
saumya-singh <saumya.singh0993@gmail.com>
2018-01-10 17:11:38 +0530

    added functionality to get all the tasks of a user and creating the final json result

--- task_model.py
+++ task_model.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
-import datetime
+import datetime, json
 import db_handler
 
 def insert_task(content, userid):
     title = content["title"]
     description = content["description"]
     deadline = content["deadline"]
-    deadline = datetime.datetime.strptime(deadline, '%d %b %Y %H:%M:%S')
+    if title == None or description == None or deadline == None:
+        result = ["Fields cannot remain empty", 0]
+        return create_json_result(result)
+    try:
+        deadline = datetime.datetime.strptime(deadline, '%d %b %Y %H:%M:%S')
+    except:
+        result = ["DEADLINE not in the right format", 0]
+        return create_json_result(result)
     task = {"title" : title, "description" : description, \
             "created_time" : datetime.datetime.utcnow(), "created_by" : userid,\
             "deadline" : deadline, "completed" : False}
-    return db_handler.insert_to_db(task)
+    result = db_handler.insert_to_db(task)
+    return create_json_result(result)
+
+def get_tasks_list(userid):
+    result = db_handler.list_tasks_from_db(userid)
+    return create_json_result(result)
+
+def create_json_result(result):
+    if result[1] == 1:
+        json_send = {"status" : "success", "data" : result[0]}
+    elif result[1] == 0:
+        json_send = {"status" : "failure", "error" : result[0]}
+    return json.dumps(json_send)
 
 if __name__ == '__main__':
     pass
