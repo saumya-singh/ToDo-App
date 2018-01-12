59b35ddb53dd5dd5be22df4ed0aa17a1eeea8c12
saumya-singh <saumya.singh0993@gmail.com>
2018-01-12 14:09:10 +0530

    separated the validation of task fields from insert_task and update_task

--- task_model.py
+++ task_model.py
@@ -2,20 +2,14 @@ import datetime, json
 import db_handler
 
 def insert_task(content, userid):
-    title = content["title"]
-    description = content["description"]
-    deadline = content["deadline"]
-    if title == None or description == None or deadline == None:
-        result = ["Fields cannot remain empty", 0]
+    res = validate_task_fields(content)
+    if res[1] == 0:
         return create_json_response(result)
-    try:
-        deadline = datetime.datetime.strptime(deadline, '%d %b %Y %H:%M:%S')
-    except:
-        result = ["DEADLINE not in the right format", 0]
-        return create_json_response(result)
-    task = {"title" : title, "description" : description, \
+    elif res[1] == 1:
+        content = res[0]
+    task = {"title" : content["title"], "description" : content["description"], \
             "created_time" : datetime.datetime.utcnow(), "created_by" : userid,\
-            "deadline" : deadline, "completed" : False}
+            "deadline" : content["deadline"], "completed" : False}
     result = db_handler.insert_to_db(task)
     return create_json_response(result)
 
@@ -28,13 +22,11 @@ def get_one_task(userid, taskid):
     return create_json_response(result)
 
 def update_task(content, userid, taskid):
-    if "deadline" in content:
-        try:
-            content["deadline"] = datetime.datetime.strptime(content["deadline"],\
-                                    '%d %b %Y %H:%M:%S')
-        except:
-            result = ["DEADLINE not in the right format", 0]
-            return create_json_response(result)
+    res = validate_task_fields(content)
+    if res is not None and res[1] == 0:
+        return create_json_response(res)
+    elif res is not None and res[1] == 1:
+        content = res[0]
     result = db_handler.update_task_in_db(content, userid, taskid)
     return create_json_response(result)
 
@@ -42,12 +34,20 @@ def delete_task(userid, taskid):
     result = db_handler.delete_task_from_db(userid, taskid)
     return create_json_response(result)
 
+def validate_task_fields(content):
+    if content["title"] in (None,"") or content["description"] in\
+                        (None,"") or content["deadline"] in (None,""):
+        return ["Fields cannot remain empty", 0]
+    try:
+        content["deadline"] = datetime.datetime.strptime(content["deadline"],\
+                                                        '%d %b %Y %H:%M:%S')
+        return [content, 1]
+    except:
+        return ["DEADLINE not in the right format", 0]
+
 def create_json_response(result):
     if result[1] == 1:
-        json_send = {"status" : "success", "data" : result[0]}
+        json_sent = {"status" : "success", "data" : result[0]}
     elif result[1] == 0:
-        json_send = {"status" : "failure", "error" : result[0]}
-    return json.dumps(json_send)
-
-if __name__ == '__main__':
-    pass
+        json_sent = {"status" : "failure", "error" : result[0]}
+    return json.dumps(json_sent)
