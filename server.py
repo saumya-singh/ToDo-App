dcef9c784921533156c58b5d50aaf6a03a4b47df
saumya-singh <saumya.singh0993@gmail.com>
2018-01-25 09:47:35 +0530

    added routes for user authentication

--- server.py
+++ server.py
@@ -1,20 +1,111 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from flask_json import FlaskJSON, JsonError, as_json
 from flask_pymongo import PyMongo
-import task_model, json
+import task_model, login_model, json
+from flask_login import LoginManager
 
 app = Flask(__name__, static_folder = 'static', static_url_path = '')
 FlaskJSON(app)
+login = LoginManager(app)
 
 app.config['MONGO_DBNAME'] = 'todo_database'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/todo_database'
 mongo = PyMongo(app)
 
-@app.route("/")
+
+# from flask import render_template, flash, redirect, url_for, request
+# from flask_login import login_user, logout_user, current_user, login_required
+# from werkzeug.urls import url_parse
+# from app import app, db
+#
+# @app.route('/')
+# @app.route('/index')
+# @login_required
+# def index():
+#     posts = [
+#         {
+#             'author': {'username': 'John'},
+#             'body': 'Beautiful day in Portland!'
+#         },
+#         {
+#             'author': {'username': 'Susan'},
+#             'body': 'The Avengers movie was so cool!'
+#         }
+#     ]
+#     return render_template('index.html', title='Home', posts=posts)
+
+
+@app.route('/api/login/', methods=['POST'])
+def login():
+	# if current_user.is_authenticated:
+    #     return redirect(url_for('index'))
+	content = request.get_json()
+	if not isinstance(content, dict):
+		raise JsonError(description='Invalid JSON')
+	result = login_model.user_validation(content)
+	return result[0], result[1]
+
+@app.route('/api/register/', methods=['POST'])
+@as_json
+def register():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('index'))
+	content = request.get_json()
+	if not isinstance(content, dict):
+		raise JsonError(description='Invalid JSON')
+	result = login_model.user_registration(content)
+	return result[0], result[1]
+
+# def login():
+# 	# if current_user.is_authenticated:
+#     #     return redirect(url_for('index'))
+# 	content = json.loads(request.get_json())
+# 	if not isinstance(content, dict):
+# 		raise JsonError(description='Invalid JSON')
+# 	result = login_model.user_validation(content)
+# 	return result[0], result[1]
+#
+#
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash('Invalid username or password')
+#             return redirect(url_for('login'))
+#         login_user(user, remember=form.remember_me.data)
+#         next_page = request.args.get('next')
+#         if not next_page or url_parse(next_page).netloc != '':
+#             next_page = url_for('index')
+#         return redirect(next_page)
+#     return render_template('login.html', title='Sign In', form=form)
+
+
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
+#
+#
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         user = User(username=form.username.data, email=form.email.data)
+#         user.set_password(form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Congratulations, you are now a registered user!')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/')
 def home_page():
 	return render_template('index.html')
 
-@app.route("/users/123/tasks/")
+@app.route('/users/123/tasks/')
 def task_page():
 	return render_template('task.html')
 
