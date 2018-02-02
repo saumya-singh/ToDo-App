5ff19587967824e75cfdf939a0d7f6aa47b480da
saumya-singh <saumya.singh0993@gmail.com>
2018-02-02 11:13:31 +0530

    added view for logout and to redirect the page on invalid requests

--- server.py
+++ server.py
@@ -1,149 +1,107 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, session, redirect
 from flask_json import FlaskJSON, JsonError, as_json
 from flask_pymongo import PyMongo
-import task_model, login_model, json
-from flask_login import LoginManager
+import task_model, login_model, json, os
 
 app = Flask(__name__, static_folder = 'static', static_url_path = '')
 FlaskJSON(app)
-login = LoginManager(app)
 
 app.config['MONGO_DBNAME'] = 'todo_database'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/todo_database'
 mongo = PyMongo(app)
 
+@app.route('/')
+def register_page():
+	return render_template('register.html')
 
-# from flask import render_template, flash, redirect, url_for, request
-# from flask_login import login_user, logout_user, current_user, login_required
-# from werkzeug.urls import url_parse
-# from app import app, db
-#
-# @app.route('/')
-# @app.route('/index')
-# @login_required
-# def index():
-#     posts = [
-#         {
-#             'author': {'username': 'John'},
-#             'body': 'Beautiful day in Portland!'
-#         },
-#         {
-#             'author': {'username': 'Susan'},
-#             'body': 'The Avengers movie was so cool!'
-#         }
-#     ]
-#     return render_template('index.html', title='Home', posts=posts)
-
-
-@app.route('/api/login/', methods=['POST'])
-def login():
-	# if current_user.is_authenticated:
-    #     return redirect(url_for('index'))
-	content = request.get_json()
-	if not isinstance(content, dict):
-		raise JsonError(description='Invalid JSON')
-	result = login_model.user_validation(content)
-	return result[0], result[1]
+@app.route('/login/')
+def login_page():
+	return render_template('login.html')
 
 @app.route('/api/register/', methods=['POST'])
 @as_json
 def register():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('index'))
 	content = request.get_json()
 	if not isinstance(content, dict):
 		raise JsonError(description='Invalid JSON')
 	result = login_model.user_registration(content)
 	return result[0], result[1]
 
-# def login():
-# 	# if current_user.is_authenticated:
-#     #     return redirect(url_for('index'))
-# 	content = json.loads(request.get_json())
-# 	if not isinstance(content, dict):
-# 		raise JsonError(description='Invalid JSON')
-# 	result = login_model.user_validation(content)
-# 	return result[0], result[1]
-#
-#
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user is None or not user.check_password(form.password.data):
-#             flash('Invalid username or password')
-#             return redirect(url_for('login'))
-#         login_user(user, remember=form.remember_me.data)
-#         next_page = request.args.get('next')
-#         if not next_page or url_parse(next_page).netloc != '':
-#             next_page = url_for('index')
-#         return redirect(next_page)
-#     return render_template('login.html', title='Sign In', form=form)
-
-
-# @app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for('index'))
-#
-#
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, email=form.email.data)
-#         user.set_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Congratulations, you are now a registered user!')
-#         return redirect(url_for('login'))
-#     return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/')
-def home_page():
-	return render_template('index.html')
-
-@app.route('/users/123/tasks/')
-def task_page():
-	return render_template('task.html')
-
-@app.route('/api/users/<userid>/tasks/', methods = ["POST"])
+@app.route('/api/login/', methods=['POST'])
 @as_json
-def insert(userid):
-	content = json.loads(request.get_json())
-	if not isinstance(content, dict):
-		raise JsonError(description='Invalid JSON')
-	result = task_model.insert_task(content, userid)
-	return result[0], result[1]
-
-@app.route('/api/users/<userid>/tasks/', methods = ["GET"])
-@as_json
-def get_all_tasks(userid):
-	result = task_model.get_tasks_list(userid)
-	return result[0], result[1]
-
-@app.route('/api/users/<userid>/tasks/<taskid>/', methods = ["GET"])
-@as_json
-def get_task(userid, taskid):
-	result = task_model.get_one_task(userid, taskid)
-	return result[0], result[1]
-
-@app.route('/api/users/<userid>/tasks/<taskid>/', methods = ["PUT"])
-@as_json
-def update(userid, taskid):
+def login():
 	content = request.get_json()
 	if not isinstance(content, dict):
 		raise JsonError(description='Invalid JSON')
-	result = task_model.update_task(content, userid, taskid)
+	result = login_model.user_validation(content)
+	if result[1] == 200:
+		session['userid'] = result[0]['data']['_id']
 	return result[0], result[1]
 
-@app.route('/api/users/<userid>/tasks/<taskid>/', methods = ["DELETE"])
-@as_json
-def delete(userid, taskid):
-	result = task_model.delete_task(userid, taskid)
-	return result[0], result[1]
+@app.route('/logout/')
+def logout():
+	if 'userid' in session:
+		session.pop('userid', None)
+	return "redirect", 302
+
+@app.route('/tasks/')
+def task_page():
+	if 'userid' in session:
+		return render_template('task.html')
+	else:
+		return render_template('login.html')
+
+@app.route('/api/tasks/', methods = ["POST"])
+def insert():
+	if 'userid' in session:
+		userid = session.get('userid')
+		content = json.loads(request.get_json())
+		if not isinstance(content, dict):
+			raise JsonError(description='Invalid JSON')
+		result = task_model.insert_task(content, userid)
+		return result[0], result[1]
+	else:
+		return redirect('/login/', 302)
+
+@app.route('/api/tasks/', methods = ["GET"])
+def get_all_tasks():
+	if 'userid' in session:
+		userid = session.get('userid')
+		result = task_model.get_tasks_list(userid)
+		return result[0], result[1]
+	else:
+		return redirect('/login/', 302)
+
+@app.route('/api/tasks/<taskid>/', methods = ["GET"])
+def get_task(taskid):
+	if 'userid' in session:
+		userid = session.get('userid')
+		result = task_model.get_one_task(userid, taskid)
+		return result[0], result[1]
+	else:
+		return redirect('/login/', 302)
+
+@app.route('/api/tasks/<taskid>/', methods = ["PUT"])
+def update(taskid):
+	if 'userid' in session:
+		userid = session.get('userid')
+		content = request.get_json()
+		if not isinstance(content, dict):
+			raise JsonError(description='Invalid JSON')
+		result = task_model.update_task(content, userid, taskid)
+		return result[0], result[1]
+	else:
+		return redirect('/login/', 302)
+
+@app.route('/api/tasks/<taskid>/', methods = ["DELETE"])
+def delete(taskid):
+	if 'userid' in session:
+		userid = session.get('userid')
+		result = task_model.delete_task(userid, taskid)
+		return result[0], result[1]
+	else:
+		return redirect('/login/', 302)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.secret_key = os.urandom(12)
+	app.run(debug = True)
